4ad4395ff1210437ba52fc2cab5d34ea33268d4b
hollyrobson <hrobson@andigital.com>
2015-05-01 11:46:14 +0100

    Initial code commit. Window with User and Password text fields and 2 buttons.

---
+++ login/src/login/Login.java
@@ -0,0 +1,12 @@
+package login;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		LoginGUI loginScreen = new LoginGUI();
+		loginScreen.createLoginGUI();
+		System.out.println("Done.");
+	}
+
+}
